92fab19442ee9075adad8bfd5b20776f9e7ae69b
Helmuth Mikkelsen <helmuthm@gmail.com>
2018-02-26 07:41:45 +0100

    Create Danish.php

---
+++ themes/Czechia/locale/Danish.php
@@ -0,0 +1,33 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: Czechia/locale/Danish.php
+| Author: Helmuth Mikkelsen (helmuth)
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['pfc_title'] = 'Czechia tema';
+$locale['pfc_description'] = 'Tema til den officielle tjekkiske support.';
+$locale['created_by']  = 'Oprettet af';
+$locale['01'] = 'IP-adresse';
+$locale['02'] = 'Om os';
+$locale['03'] = 'Seneste nyheder';
+$locale['04'] = 'Ingen nyheder oprettet endnu';
+$locale['05'] = 'Læs mere';
+$locale['06'] = 'Officiel NSS';
+$locale['07'] = 'Officiel hovedsupport';
+$locale['08'] = 'Kategori';
+$locale['09'] = 'Download';
+$locale['10'] = 'Udgivers beskrivelse';
+$locale['11'] = 'Detaljerede skærmdumps';
+$locale['12'] = 'Af';
+$locale['13'] = 'Seneste kommentarer';
